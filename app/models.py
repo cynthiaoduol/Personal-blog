58b889b96ee0fede070a5ee36733fd5eef97f42c
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-28 17:20:27 +0300

    add new column to the User class model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pass_hash = db.Column(db.String(255))
+    
     blogs = db.relationship('Blog', backref='user', lazy='dynamic')
 
     def __repr__(self):
