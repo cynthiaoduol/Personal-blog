43c3d6e6982e06a48deb2fc4c8170c7447d57eff
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-28 17:26:40 +0300

    raise an AttributeError to block access to the password property

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     pass_hash = db.Column(db.String(255))
-    
+
     blogs = db.relationship('Blog', backref='user', lazy='dynamic')
 
     def __repr__(self):
@@ -20,6 +20,19 @@ class Blog(db.Model):
     title = db.Column(db.String(255))
     content = db.Column(db.String())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_hash = generate_password_hash(password)
+
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_hash, password)
+
 
     def __repr__(self):
         return f'Blog {self.title}'
