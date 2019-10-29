38869bc39ac4b7ca1c38a55d62eaf93089452f2c
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-29 10:50:33 +0300

    create a new class for new blogs

--- app/main/forms.py
+++ app/main/forms.py
@@ -11,5 +11,23 @@ class BlogForm(FlaskForm):
     submit = SubmitField('Post Blog')
 
 class UpdateProfile(FlaskForm):
+    username = StringField('Enter Your Username', validators=[Required()])
+    email = StringField('Email Address', validators=[Required(),Email()])
     bio = TextAreaField('Tell us something about you.',validators = [Required()])
+    profile_picture = FileField('profile picture', validators=[FileAllowed(['jpg','png'])])
     submit = SubmitField('Submit')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            if User.query.filter_by(email = email.data).first():
+                raise ValidationError("The Email has already been taken!")
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            if User.query.filter_by(username = username.data).first():
+                raise ValidationError("The username has already been taken")
+
+class CreateBlog(FlaskForm):
+    title = StringField('Title',validators=[Required()])
+    content = TextAreaField('Blog Content',validators=[Required()])
+    submit = SubmitField('Post')
