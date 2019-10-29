1d90d1bc6537e2f2f02b0d7c924817eb9dfb7673
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-29 10:52:00 +0300

    create new models

--- app/models.py
+++ app/models.py
@@ -4,6 +4,17 @@ from flask_login import UserMixin
 from datetime import datetime
 from . import login_manager
 
+class Quote:
+    '''
+    Quote class to define the quotes objects
+    
+    '''
+    
+    def __init__(self,author,quote):
+        self.author = author
+        self.quote = quote
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -19,7 +30,8 @@ class User(UserMixin,db.Model):
     profile_pic_path = db.Column(db.String())
     pass_hash = db.Column(db.String(255))
 
-    blogs = db.relationship('Blog', backref='user', lazy='dynamic')
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
 
     @property
     def password(self):
@@ -35,6 +47,7 @@ class User(UserMixin,db.Model):
     def verify_password(self, password):
         return check_password_hash(self.pass_hash, password)
 
+
     def __repr__(self):
         return f'User {self.username}'  
 
@@ -46,6 +59,7 @@ class Blog(db.Model):
     content = db.Column(db.String(),nullable=False)
     posted_on = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comment = db.relationship('Comment', backref='blog', lazy='dynamic')
 
     def save(self):
         db.session.add(self)
@@ -62,4 +76,43 @@ class Blog(db.Model):
         return blog
 
     def __repr__(self):
-        return f'Blog {self.title}'
+        return f"Blog ('{self.title}','{self.posted_on}')"
+
+
+class Comment(db.Model):
+    __tablename__='comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    blog_id = db.Column(db.Integer,db.ForeignKey("blogs.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.remove(self)
+        db.session.commit()
+
+    def get_comment(id):
+        comment = Comment.query.all(id=id)
+        return comment
+
+
+    def __repr__(self):
+        return f'Comment {self.comment}'
+
+class Subscriber(db.Model):
+    __tablename__='subscribers'
+
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Subscriber {self.email}'
