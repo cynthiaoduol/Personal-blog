6499c36972f1eef4960183b87da987539478da0e
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-29 10:47:25 +0300

    ent email to user when they subscribe

--- app/auth/views.py
+++ app/auth/views.py
@@ -33,7 +33,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to G~Blog","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to G~Blog","email/subscriber",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
